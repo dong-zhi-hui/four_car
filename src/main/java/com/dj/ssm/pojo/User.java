4265f2a629eb3f3ca442ef7332c34ec1b82168f8
zhw <897272497@qq.com>
2020-07-22 15:37:47 +0800

    添加user字段

--- src/main/java/com/dj/ssm/pojo/User.java
+++ src/main/java/com/dj/ssm/pojo/User.java
@@ -1,8 +1,53 @@
 package com.dj.ssm.pojo;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Data
+@TableName("user")
 public class User {
 
+    /**
+     * 主键id
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 用户名
+     */
+    private String userName;
+
+    /**
+     * 密码
+     */
+    private String userPwd;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 车牌号
+     */
+    private String carNumber;
+
+    /**
+     * 用户状态
+     */
+    private Integer userStatus;
+
+    /**
+     * 创建时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createTime;
 }
